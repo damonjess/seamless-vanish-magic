d3c57f6f86a10f064d5788a4e9186648199bddf9
damonjess <damonkirby86@gmail.com>
2026-09-07 15:04:22 +0100

    Add Capacitor and native Android project integration for Android Studio

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.vanish',
+  appName: 'Perfect Eraser',
+  webDir: '.output/public'
+};
+
+export default config;
